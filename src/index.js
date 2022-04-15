eb15ca5d5ce90ab89127409190850290e651ba7f
Anas <anasbadr23@gmail.com>
2022-04-15 22:33:32 +0530

    Fragments

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
 
-
+function AppTwo(){
+    return(
+<h1> This is second app </h1>
+    );
+}
 
 ReactDOM.render(
-<App/>,
+<>
+<App/>
+<AppTwo/>
+</>,
 document.getElementById("root")
 );
 

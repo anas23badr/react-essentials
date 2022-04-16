537e332c19e8467fd4ef99879e92e32ac35a7ae0
Anas <anasbadr23@gmail.com>
2022-04-16 14:25:51 +0530

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 
-function SecretComponent(){
-  return <h1>Secret information for authorized users only</h1>
-}
-function RegularComponent(){
-  return <h1>Everyone can see this component</h1>
-}
-function App({authorized}) {//props object destructuring {property1, property2,...}
-  
-    /*if(props.authorized){
-      return <SecretComponent/>
-    }
-    else{
-      return <RegularComponent/>
-    }*/
-    return(
-      <>
-      {authorized? <SecretComponent/> : <RegularComponent/>}
-      </>
-    )
-   
-  
+function App() {
+  const [emotion, setEmotion]= useState("happy");  // happy is the initial state stored in emotion. setEmotion is the function used to change state of emotion 
+  return(
+    <>
+    <h1>Current emotion is {emotion}.</h1>
+    <button onClick={()=>setEmotion("happy")}>Happy</button>
+    <button onClick={()=>setEmotion("Frustrated")}>Frustrate</button>
+    <button onClick={()=>setEmotion("Enthusiastic")}>Enthuse</button>
+    </>
+  )
 }
 
 export default App;

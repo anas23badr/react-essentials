4b044293538d0910ff7200cbc4f1ccc64d9f483c
Anas <anasbadr23@gmail.com>
2022-04-17 19:43:55 +0530

    React Router-Nesting links and displaying nested components with Outlet component form react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
-import{Home, About, Events, Contact, Whoops404} from './pages.js'
+import{Home, About, Events, Contact, Whoops404,Services,CompanyHistory,Location} from './pages.js'
 
 function App() {
     return(
       <div>
         <Routes>
-          <Route path="react-essentials/" element={<Home />}></Route>
-          <Route path="react-essentials/about" element={<About />}></Route>
-          <Route path="react-essentials/events" element={<Events />}></Route>
-          <Route path="react-essentials/contact" element={<Contact />}></Route>
+          <Route path="react-essentials/" element={<Home />}/>
+          <Route path="react-essentials/about" element={<About />}>
+            <Route path="services" element={<Services/>}/>
+            <Route path="companyhistory" element={<CompanyHistory/>}/>
+            <Route path="location" element={<Location/>}/>
+          </Route>
+          <Route path="react-essentials/events" element={<Events />}/>
+          <Route path="react-essentials/contact" element={<Contact />}/>
           <Route path="*" element={<Whoops404 />}></Route>
         </Routes>
       </div>

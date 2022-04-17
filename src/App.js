58f65d11ba096b8b392e19e4969013a36a73fe3f
Anas <anasbadr23@gmail.com>
2022-04-17 15:47:38 +0530

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 
 function App() {
-  const [emotion, setEmotion]= useState("happy");  // happy is the initial state stored in emotion. setEmotion is the function used to change state of emotion 
+  const [emotion, setEmotion]= useState("happy");  
+  const [secondary, setSecondary]= useState("tired");
+  useEffect(()=>{
+    console.log(`It's ${emotion} around here!`);
+  }, [emotion]); //useEffect is used to track the change in  state , a call back funtion is called when the state changes..it can be used to console log the cahnges or can be used for effects
+  useEffect(()=>{
+    console.log(`It's ${secondary} around here!`);
+  }, [secondary]);
   return(
     <>
-    <h1>Current emotion is {emotion}.</h1>
-    <button onClick={()=>setEmotion("happy")}>Happy</button>
+    <h1>Current emotion is {emotion} and {secondary}.</h1>
+    <button onClick={()=>setEmotion("happy")}>Make Happy</button>
+    <button onClick={()=>setSecondary("Crabby")}>Make Crabby</button>
     <button onClick={()=>setEmotion("Frustrated")}>Frustrate</button>
     <button onClick={()=>setEmotion("Enthusiastic")}>Enthuse</button>
     </>

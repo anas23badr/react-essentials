f303ef61dba2ec4de96de4c5e63ee68222d5db58
Anas <anasbadr23@gmail.com>
2022-04-17 18:11:17 +0530

    Loading state handling and error handling on rendering

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import './App.css';
 
 // https://api.github.com/users/anas23badr
 function App({login}) {
-  console.log(login);
   const [data, setData]=useState(null);
+  const[loading, setLoading]=useState(false);
+  const[error, setError]= useState(null);
+  
   useEffect(()=>{
+    if(!login) return;
+    setLoading(true);
     fetch(`https://api.github.com/users/${login}`)
     .then(response=> response.json())
-    .then(setData);
-  }, []); //we only want fetch data only once on page load thats why []
+    .then(setData)
+    .then(setLoading(false))
+    .catch(setError);
+  }, [login]);
+  
+  if (loading)
+  return <h1>Loading..</h1>
+  if(error)
+  return <pre>{JSON.stringify(error, null ,2)}</pre>
   if (data){
     return(
       <div>
       <h1>{data.login}</h1>
-      <img src={data.avatar_url}/>
+      <img src={data.avatar_url} alt="your profile"/>
       </div>
     )
   } 

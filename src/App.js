420e8d157e8fadb87595d27345d5d357816fb750
Anas <anasbadr23@gmail.com>
2022-04-17 19:23:07 +0530

    React Router-Incoperating link components and useLocation hook from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
-import{Home, About, Events, Contact} from './pages.js'
+import{Home, About, Events, Contact, Whoops404} from './pages.js'
 
 function App() {
     return(
@@ -11,6 +11,7 @@ function App() {
           <Route path="react-essentials/about" element={<About />}></Route>
           <Route path="react-essentials/events" element={<Events />}></Route>
           <Route path="react-essentials/contact" element={<Contact />}></Route>
+          <Route path="*" element={<Whoops404 />}></Route>
         </Routes>
       </div>
     )
